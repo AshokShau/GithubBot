979b446ddff4b8d5124b50b642ad4fba8a01139a
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-01-06 12:59:46 +0530

    build(deps): Bump go-github from v80 to v81

--- internal/bot/commands/commands.go
+++ internal/bot/commands/commands.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/google/go-github/v80/github"
+	"github.com/google/go-github/v81/github"
 )
 
 type CommandHandler struct {
