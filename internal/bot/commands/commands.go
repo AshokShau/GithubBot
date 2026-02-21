0ece9cebf3f6793053777787e779ef884507c478
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-02-21 09:09:02 +0530

    Bump go-github from v82 to v83 (#15)

--- internal/bot/commands/commands.go
+++ internal/bot/commands/commands.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/google/go-github/v82/github"
+	"github.com/google/go-github/v83/github"
 )
 
 type CommandHandler struct {
