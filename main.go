250b070d3be4243fc4537e512dfb6a339e7f78cc
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-08-29 11:20:58 +0530

    time.Minute WTF ?

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 
 	bot, err := gotdbot.NewClient(6, "eb06d4abfb49dc3eeb1aeb98ae0f581e", cfg.TelegramToken, &gotdbot.ClientOpts{
 		LibraryPath: "./libtdjson.so.1.8.67",
-		AutoRetry:   &gotdbot.AutoRetry{MaxFloodWait: 8 * time.Minute, ChatNotFound: true},
+		AutoRetry:   &gotdbot.AutoRetry{MaxFloodWait: 10 * time.Second, ChatNotFound: true},
 	})
 
 	if err != nil {
